bf8392f5f737d8288a3396af658c138f0c79c023
Waqas Bin Niaz <140584666+Waqas2003@users.noreply.github.com>
2025-07-08 17:24:58 +0500

    Add src\components\Dashboard.js

---
+++ src\components\Dashboard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Dashboard.css';
+
+function Dashboard() {
+  return (
+    <main className="dashboard">
+      <h2>Dashboard</h2>
+      <p>Welcome to the POS system dashboard!</p>
+    </main>
+  );
+}
+
+export default Dashboard;
